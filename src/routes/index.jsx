6bfa2a26724179939813b6b798651fb6c4294001
QuizitoCristiano <quizitocristiano10@gmail.com>
2024-03-11 20:32:30 -0300

    sad

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,40 +1,29 @@
 import React, { useContext, useRef } from "react";
 import { MainLayout } from "../layouts/mylayouts";
-import { BrowserRouter, Routes, Route, Link, useNavigation } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  useNavigation,
+} from "react-router-dom";
 import { MyHome } from "../pages/home";
 import { SignIn } from "../componets/signin-in/signinIn";
 import { AuthContext } from "../authcontext";
 
-const myArrayLinks = [
-    '/'
-]
+const myArrayLinks = ["/"];
 export const MainRoutes = () => {
-    const pathname = useRef()
-    const { isLoggedIn } = useContext(AuthContext)
-    if (isLoggedIn && myArrayLinks.includes(pathname.current)) {
-        return (
+  const pathname = useRef();
+  const { isLoggedIn } = useContext(AuthContext);
+  if (isLoggedIn && myArrayLinks.includes(pathname.current)) {
+    return <Route path="/" element={<MyHome />} />;
+  }
 
-          
-                    <Route path="/" element={<MyHome />} />
-
-            
-            
-
-        )
-
-    }
-
-    return (
-        <MainLayout>
-            
-         
-                <Routes>
-                    <Route path="/" element={<MyHome />} />
-                
-                </Routes>
-               
-           
-        </MainLayout>
-    )
-
-}
+  return (
+    <MainLayout>
+      <Routes>
+        <Route path="/" element={<MyHome />} />
+      </Routes>
+    </MainLayout>
+  );
+};
