4272d1c99d25214e8b031b62c1effc6b1683266e
QuizitoCristiano <quizitocristiano10@gmail.com>
2024-10-19 22:34:13 -0300

    Operd

--- src/componets/category/category.jsx
+++ src/componets/category/category.jsx
@@ -1,20 +1,17 @@
-import React from 'react';
-import { TextField, Typography, Stack, Box } from '@mui/material';
-import './styles/myCategory.css';
+import React from 'react'
+import { TextField, Typography, Stack, Box } from '@mui/material'
+import './styles/myCategory.css'
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
 
-import manga from '../../images/manga.jpg';
-import morango from '../../images/morango.jpg';
-import Uva1 from '../../images/Uva1.png';
-import abacate from '../../images/abacate.jpg';
-import papaia from '../../images/papaia.png';
-
-
+import manga from '../../images/manga.jpg'
+import morango from '../../images/morango.jpg'
+import Uva1 from '../../images/Uva1.png'
+import abacate from '../../images/abacate.jpg'
+import papaia from '../../images/papaia.png'
 
 const CategoryItem = [
   {
     nome: 'Abacate e Manga',
-    descricao: '22 Item',
     img: manga,
     backgroundColor: '#fef4ea',
   },
@@ -22,148 +19,124 @@ const CategoryItem = [
     nome: 'Morango',
     descricao: '19 Item',
     img: morango,
-    backgroundColor: '#e9f4e3', 
+    backgroundColor: '#e9f4e3',
   },
   {
     nome: 'Uva',
-    descricao: '13 Item',
     img: Uva1,
-    backgroundColor: '#faeaea', 
+    backgroundColor: '#faeaea',
   },
   {
     nome: 'Abacate',
-    descricao: '9 Item',
     img: abacate,
-    backgroundColor: '#eeeef9', 
+    backgroundColor: '#eeeef9',
   },
   {
     nome: 'Mamão ou Papaia',
-    descricao: '22 Item',
     img: papaia,
-    backgroundColor: '#f7f6d7', 
+    backgroundColor: '#f7f6d7',
   },
-];
-
-// delicacy-company 
-
-
+]
 
 export const Category = () => {
-  const newCategoryItem = CategoryItem.map((item, index) => {
-    return (
-      <>
-      <Stack
-        key={index}
-        sx={{
-          width: '100%',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          flexDirection: 'column',
-          gap: '2rem',
-         
-          '@media only screen and (max-width: 305px)': {
-            gap: '1rem',
-          },
-        }}
-      >
-        <Box
-          className="box"
-          sx={{
-            width: '100%',
-            backgroundColor: item.backgroundColor,
-            display: 'flex',
-         
-            flexDirection: 'column',
-            alignItems: 'center',
-        
-            borderRadius: '0.5rem',
-            '& img': {
-              width: '100%',
-              height: '100px',
-              objectFit: 'contain',
-              objectPosition: 'center',
-            },
-            '& h2': {
-              fontSize: '1.7rem',
-              color: 'black',
-              fontWeight: 600,
-            },
-            '& span': {
-              fontSize: '0.8rem',
-              fontWeight: 400,
-              marginBottom: '1rem',
-            },
-            '& .bx': {
-              padding: '10px',
-              color: 'var(--bg-color)',
-              borderRadius: '5rem',
-              marginTop: '2rem',
-              position: 'absolute',
-              bottom: '-8%',
-              display: 'none',
-            },
-            '&:hover .bx': {
-              display: 'block',
-              transition: '0.5s all linear',
-            },
-          }}
-        >
-          <img src={item.img} alt="Opa!! Não deu certo, tenta novamente." />
-          <Typography className='textName'>{item.nome}</Typography>
-          <p>{item.descricao}</p>
-          <ArrowForwardIcon sx={{
-            color: 'var(--bg-color)',
-            fontSize: '3.8rem',
-         
-          }} className="bx"/>
-        </Box>
-      </Stack>
-      </>
-    );
-  });
+  const newCategoryItem = CategoryItem.map((item, index) => (
+    <Box
+      key={index}
+      className="ItemMubox"
+      sx={{
+        width: '100%',
+        maxWidth: '250px',
+        backgroundColor: item.backgroundColor,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        borderRadius: '2.5rem',
+        padding: '1rem',
+        gap: '0.5rem',
+        position: 'relative',
+
+        '& img': {
+          width: '80%',
+          height: '100px',
+          objectFit: 'contain',
+        },
+
+        '& h2': {
+          fontSize: '1.5rem',
+          color: 'black',
+          fontWeight: 600,
+        },
+
+        '& span': {
+          fontSize: '0.8rem',
+          fontWeight: 400,
+          marginBottom: '1rem',
+        },
+
+        '& .bx': {
+          padding: '10px',
+          color: 'var(--bg-color)',
+          borderRadius: '50%',
+          marginTop: '2rem',
+          position: 'absolute',
+          bottom: '-8%',
+          display: 'none',
+        },
+
+        '&:hover .bx': {
+          display: 'block',
+          transition: '0.5s all linear',
+        },
+      }}
+    >
+      <img src={item.img} alt={`Imagem de ${item.nome}`} />
+      <Typography className="textName">{item.nome}</Typography>
+      <p>{item.descricao}</p>
+      <ArrowForwardIcon
+        sx={{ color: 'var(--bg-color)', fontSize: '3.2rem' }}
+        className="bx"
+      />
+    </Box>
+  ))
 
   return (
-    <Stack className="categoria">
-      <Stack className="heading">
-        <h1>
+    <Stack className="categoria" sx={{ width: '100%' }}>
+      <Stack
+        className="heading"
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'flex-start',
+          gap: '2rem',
+          width: '100%',
+        }}
+      >
+        <h1 style={{ fontSize: '2.5rem', fontWeight: 700, marginLeft: '1rem' }}>
           Explorando a Diversidade da Natureza <br />
-          <span>Frutas</span>
+          <span style={{ fontSize: '2rem', fontWeight: 700 }}>Frutas</span>
         </h1>
         <a href="#" className="btnMyButton">
-          Ver tudo <ArrowForwardIcon className="bx"/>
+          Ver tudo <ArrowForwardIcon className="bx" />
         </a>
       </Stack>
 
-
-      
-
-      {/* container content  */}
+      {/* Container para os Cards */}
       <Stack
-        className="categoria-container"
         sx={{
           display: 'grid',
-          width:'100',
           gridTemplateColumns: 'repeat(auto-fit, minmax(180px, 1fr))',
           gap: '1rem',
+          width: '100%',
           marginTop: '2rem',
-         
-        
-          alignItems: 'center',
-          justifyContent: 'center', 
-          '@media only screen and (max-width: 500px)': {
-         
-            width: '100%',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          
-            gridTemplateColumns: '1fr',
+          justifyContent: 'center',
+
+          '@media (max-width: 500px)': {
+            gridTemplateColumns: 'repeat(2, 1fr)',
           },
         }}
       >
         {newCategoryItem}
       </Stack>
     </Stack>
-  );
-};
+  )
+}
